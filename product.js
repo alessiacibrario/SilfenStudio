40c9cd5e5b900f78e3cc62f9beccb8f8ed40ed64
alessiacibrario <alessia@cibrario.org>
2022-05-03 15:21:17 +0200

    js2

--- product.js
+++ product.js
@@ -1,23 +1,22 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("_id");
-const url = "http://marinedekoninck.com/silfen/wordpress-2/";
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get("_id");
+// const url = "http://marinedekoninck.com/silfen/wordpress-2/";
 
-fetch(url, options)
-  .then((res) => res.json())
-  .then((data) => showBag(data));
-
-const options = {
-  headers: {
-    "x-apikey": "",
-  },
-};
+function getData() {
+  fetch("http://marinedekoninck.com/silfen/wordpress-2/wp-json/wp/v2/posts")
+    .then((res) => res.json())
+    .then((data) => showBag(data));
+}
+// const options = {
+//   headers: {
+//     "x-apikey": "",
+//   },
+// };
 
 //populate the page
 function showBag(bag) {
-  function showBag(bag) {
-    console.log(bag);
-    document.querySelector(".bagName").textContent = bag.name;
-    document.querySelector(".bagPrice").textContent = "KK ${bag.price}";
-    document.querySelector(".bagImg img").src = bag.img;
-  }
+  console.log(bag);
+  document.querySelector(".bagName").textContent = bag.name;
+  document.querySelector(".bagPrice").textContent = "KK ${bag.price}";
+  document.querySelector(".bagImg img").src = bag.img;
 }
