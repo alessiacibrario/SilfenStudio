877cd9f37b9b3b610f3aaa2975378562a765fab2
alessiacibrario <alessia@cibrario.org>
2022-05-03 16:31:33 +0200

    js3

--- product.js
+++ product.js
@@ -2,21 +2,26 @@
 // const id = urlParams.get("_id");
 // const url = "http://marinedekoninck.com/silfen/wordpress-2/";
 
-function getData() {
-  fetch("http://marinedekoninck.com/silfen/wordpress-2/wp-json/wp/v2/posts")
-    .then((res) => res.json())
-    .then((data) => showBag(data));
-}
+// function getData() {
+// .then((data) => showBag(data));
+// }
 // const options = {
 //   headers: {
 //     "x-apikey": "",
 //   },
 // };
 
+fetch("http://marinedekoninck.com/silfen/wordpress-2/wp-json/wp/v2/posts")
+  .then((res) => res.json())
+  .then((data) => showBag(data));
+
 //populate the page
 function showBag(bag) {
   console.log(bag);
-  document.querySelector(".bagName").textContent = bag.name;
+  document.querySelector(".bagName").textContent = bag.title;
+  document.querySelector(".bagInfo").textContent = bag.rendered;
   document.querySelector(".bagPrice").textContent = "KK ${bag.price}";
-  document.querySelector(".bagImg img").src = bag.img;
+  document.querySelector(".bagImg img").src = bag.link;
 }
+
+showBag(bag);
